8bb484f5321895264d36f8706a9e3df968edd9d2
Truls Andreas Berglund <trulsabe@stud,.ntnu.no>
2018-09-05 12:46:28 +0200

    Added feature _countFiles, for use in the text in the box. Added comments where needed.

--- pro2/src/components/Audio.js
+++ pro2/src/components/Audio.js
@@ -8,23 +8,61 @@ class Audio extends Component {
     constructor(props) {
         super(props);
         this._playSound = this._playSound.bind(this);
+
+        this.state = {
+            audioHund: "hundbjeff.wav",
+            audioKatt: "",
+            audioElefant: "ElefantSnabelLyd.mp3"
+        }
+
+
     }
 
     render() {
         return (
             <div>
                 <button onClick={this._playSound()}>
-                    Play Sounds
+                    Play {this._countFiles()} sound(s)
                 </button>
             </div>
         )
     }
 
+    /**
+     * Plays sound files that are stored in the
+     * state. The files are played sequentially.
+     * @private
+     */
     _playSound() {
-
+        // TODO: Make the audio play in the browser
+        for (let i = 0; i < this.state.length ; i++) {
+            let audio_file = this.state[i];
+            if (audio_file !== "") {
+                let audio = new Audio(audio_file);
+                audio.play();
+            }
+        }
     }
 
-}
+    /**
+     * Counts the number of valid audio files
+     * in the state.
+     * @returns {number}
+     * @private
+     */
+    _countFiles() {
+        let count = 0;
+        let ts = this.state;
+        for (let key in ts) {
+            if (ts.hasOwnProperty(key) && ts[key] !== "") {
+                    count++;
+            }
+        }
+        return count;
+    }
 
+
+
+}
 
 export default Audio;
