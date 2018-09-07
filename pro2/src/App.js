50bbc3ebad7da7ddeed3d9f09ca4e95271a095e8
filiphag <filiphag@stud.ntnu.no>
2018-09-07 09:36:42 +0200

    update

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Visuals from './components/visuals'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Visuals from "./components/visuals";
 
 class App extends Component {
   render() {
--- pro2/src/components/visuals.jsx
+++ pro2/src/components/visuals.jsx
@@ -1,31 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import $ from "jquery";
 
 class Visuals extends Component {
-    state = {
-        kattebilde: '../media/images/Cat2.svg',
-        hundebilde: '../images/Dog1.svg',
-        elefantBilde: '../images/Elephant1.svg'
+  state = {
+    url: "Example.svg"
+  };
 
-    };
+  constructor() {
+    super();
+  }
 
+  imageStyle = {
+    width: 200,
+    height: 200
+  };
 
-    constructor() {
-        super();
-    }
+  funksjon() {
+    console.log("hei");
+    fetch("Example.svg").then(function(response) {
+      // perform setState here
+      $("#bilde").append(response);
+    });
+  }
 
-    imageStyle = {
-        width: 200,
-        height: 200
-    };
+  render() {
+    return (
+      <div>
+        <div id="bilde">
+          <svg>
+            <circle cx={50} cy={50} r={10} fill="yellow" />
+          </svg>
+        </div>
+        <button
+          id="knapp"
+          onClick={() => {
+            this.funksjon();
+          }}
+        >
+          Klikk meg!
+        </button>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            <div style={this.imageStyle}>
-                <img src={this.state.kattebilde} />
-            </div>
-        );
-    }
-
-};
+$(document).ready(function() {
+  $("#knapp").on("click", "button.switch", function() {
+    console.log("hei");
+    $.ajax("Example.svg").done(function(response) {
+      $("#bilde").html(response);
+    });
+  });
+});
 
 export default Visuals;
