4ec7530064092073fcce25a243d2df54c1285f76
filiphag <filiphag@stud.ntnu.no>
2018-09-10 12:36:15 +0200

    Lagde tabs med litt basic styling

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Visuals from "./components/visuals";
+import Tabs from "./components/tabs";
+import Tab from "./components/tab";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Visuals />
+        <Tabs>
+          <div label="Tab 1">
+            <Visuals />
+          </div>
+          <div label="Tab 2">Innhold 2</div>
+          <div label="Tab 3">Innhold 3</div>
+          <div label="Tab 4">Innhold 4</div>
+        </Tabs>
       </div>
     );
   }
