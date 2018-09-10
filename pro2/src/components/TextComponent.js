9e95c553aa43f3361e24d5d0517acf3c7a6cb32a
simnoba <simnoba@gmail.com>
2018-09-10 14:36:59 +0200

    Addet endrende textxomponent

--- pro2/src/components/TextComponent.js
+++ pro2/src/components/TextComponent.js
@@ -3,29 +3,61 @@ import React from 'react';
 import axios from 'axios';
 
 export default class TextComponent extends React.Component {
-    state = {
-        persons: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            dikt: [],
+            diktID: null
+        };
+
     }
 
 
     componentDidMount() {
         axios.get('./Media/TextJSON/HelloWorld.json')
             .then(res => {
-                const persons = res.data;
-                console.log(persons);
-                this.setState({persons});
+                const poemData = res.data;
+                this.setState({dikt: poemData});
             })
     }
+    catState(){
+        this.setState({
+            diktID: Math.round(Math.random()*4)
+        });
+    }
 
-
+    dogState(){
+        this.setState({
+            diktID: Math.round(Math.random()*4+4)
+        });
+    }
+    elephantState(){
+        this.setState({
+            diktID: Math.round(Math.random()*4+8)
+        });
+    }
     render() {
+
         return (
-            <ul>
-                { this.state.persons.map(person => <li key={person.id}>{person.name}</li>)}
-            </ul>
+            /*<div className={"diktTekst"}>
+                <p className={"diktTittel"}> {this.state.dikt[0]["id"]}
+                </p>
+            </div>*/
+                <React.Fragment>
+                {this.state.dikt.map(poem => {
+                        return poem.id === this.state.diktID ?
+                            <div key={poem.id} className={"diktTekst"}>
+                            <p className={"diktTittel"} >{poem.tittel}</p>
+                            <p>{poem.firstLine}</p>
+                            <p>{poem.secondLine}</p>
+                            <p>{poem.thirdLine}</p>
+                            </div>
+                            :
+                            null
+                    }
+                )
+                }
+                </React.Fragment>
         )
     }
 }
-
-
-
