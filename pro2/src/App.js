da6f0ab806d71215903219488b578951cdb68955
filiphag <filiphag@stud.ntnu.no>
2018-09-11 12:35:19 +0200

    merge conflict fix

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,37 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Visuals from "./components/Visuals";
 import Tabs from "./components/Tabs";
 
 // components
-import Tab from './components/Tab.js';
-
 
 class App extends Component {
-<<<<<<< HEAD
-    render() {
-        return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Tab/>
-          </div>
-        );
-      }
-=======
   render() {
     return (
       <div className="App">
         <Tabs>
-          <div label="Tab 1">
-            <Visuals />
-          </div>
+          <div label="Tab 1" />
           <div label="Tab 2" />
           <div label="Tab 3" />
           <div label="Tab 4" />
@@ -39,7 +18,6 @@ class App extends Component {
       </div>
     );
   }
->>>>>>> Development-tabs
 }
 
 export default App;
