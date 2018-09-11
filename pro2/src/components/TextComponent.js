07b9df9d84c781a9cfc5339550fd0728ff8aa971
simnoba <simnoba@gmail.com>
2018-09-11 13:27:01 +0200

    dev-TextComponent har lagt inn logikk for å velge ut 4 av hvert type dikt

--- pro2/src/components/TextComponent.js
+++ pro2/src/components/TextComponent.js
@@ -3,46 +3,109 @@ import React from 'react';
 import axios from 'axios';
 
 export default class TextComponent extends React.Component {
+
+    /**
+     * Setter start staten av textComponent til å ha en tom dikttabell og en null diktId, samt arrayene som skal holde kategoriene og dere dikt.
+     * @param props
+     */
     constructor(props) {
         super(props);
         this.state = {
             dikt: [],
-            diktID: null
+            diktID: null,
+            hundeState: [],
+            katteState: [],
+            elefantState: []
+
         };
 
     }
 
-
+    /**
+     * Henter inn JSON filen med diktene våre og setter dikt arrayen til å holde på disse
+     */
     componentDidMount() {
-        axios.get('./Media/TextJSON/HelloWorld.json')
+        axios.get('./media/TextJSON/HelloWorld.json')
             .then(res => {
                 const poemData = res.data;
                 this.setState({dikt: poemData});
-            })
-    }
-    catState(){
-        this.setState({
-            diktID: Math.round(Math.random()*4)
-        });
+            });
+        this.elephantState();
+        this.elephantState();
+        this.elephantState();
+        this.elephantState();
     }
 
+    /**
+     * catState er en hjelpefunksjon for å hente inn en tabell for rekkefølgen, uten gjenvalg på hvilket jsonText objekt som skal leses inn
+     * i selve textComponenten
+     */
+    catState(){
+        let index = Math.round(Math.random()*4+0.5);
+        if (this.state.katteState.indexOf(index) === -1){
+            this.setState({
+                diktID: index,
+                katteState: this.state.katteState.push(index)
+            });
+        }
+        else{
+            if (this.state.katteState.length === 4){
+            }
+            else{
+                this.catState()
+            }
+        }
+    }
+    /**
+     * dogState er en hjelpefunksjon for å hente inn en tabell for rekkefølgen, uten gjenvalg på hvilket jsonText objekt som skal leses inn
+     * i selve textComponenten
+     */
     dogState(){
-        this.setState({
-            diktID: Math.round(Math.random()*4+4)
-        });
+        let index = Math.round(Math.random()*4 +4.5);
+        if (this.state.hundeState.indexOf(index) === -1){
+            this.setState({
+                diktID: index,
+                hundeState: this.state.hundeState.push(index)
+            });
+        }
+        else{
+
+            if (this.state.hundeState.length === 4){
+            }
+            else{
+                this.dogState()
+            }
+        }
     }
+
+    /**
+     * elePhantState er en hjelpefunksjon for å hente inn en tabell for rekkefølgen, uten gjenvalg på hvilket jsonText objekt som skal leses inn
+     * i selve textComponenten
+     */
     elephantState(){
-        this.setState({
-            diktID: Math.round(Math.random()*4+8)
-        });
+        let index = Math.round(Math.random()*4 +8.5);
+        if (this.state.elefantState.indexOf(index) === -1){
+            this.setState({
+                diktID: index,
+                elefantState: this.state.elefantState.push(index)
+            });
+        }
+        else{
+            if (this.state.elefantState.length === 4){
+            }
+            else{
+                this.elephantState()
+            }
+        }
     }
+
+    /**
+     * Henter ut resultatet fra axios kallet og itererer gjennom alle alternativene helt til den som stemmer overens med diktId
+     * er valgt. SÅ skriver den ut diktet.
+     */
     render() {
 
         return (
-            /*<div className={"diktTekst"}>
-                <p className={"diktTittel"}> {this.state.dikt[0]["id"]}
-                </p>
-            </div>*/
                 <React.Fragment>
                 {this.state.dikt.map(poem => {
                         return poem.id === this.state.diktID ?
