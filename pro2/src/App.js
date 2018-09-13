c52f18bc128de214a5ba2dbacd2548bfa1e280e6
simnoba <simnoba@gmail.com>
2018-09-13 20:07:53 +0200

    Gjorde koden litt mer kompakt

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Tabs from "./components/Tab.js";
 import Visuals from "./components/Visuals.jsx";
+import TextComponent from "./components/TextComponent";
 
 // components
 
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Visuals />
+        <TextComponent/>
       </div>
     );
   }
